9fd845496deaf321a0275cee5ff81bb00324549d
Rajiv Shankar <rajiv.shankar.email@gmail.com>
2018-09-04 20:29:25 -0400

    update

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,25 +5,26 @@ const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
 
-// Set initial menu state
+// Set initial menu state (set value of var 'showMenu' to false)
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-  if(!showMenu) {
-    menuBtn.classList.add("close");
-    menuBtn.classList.add("show");
+  if (!showMenu) {  // if showMenu = false (initial state)
+    // 'classList' property returns the class name(s) of element as DOMTokenList object
+    menuBtn.classList.add("close");  // add "close" class name to <div class="menu-btn">
+    menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
 
-    // Set menu state
+    // Change menu state
     showMenu = true;
 
   } else {
     menuBtn.classList.remove("close");
-    menuBtn.classList.remove("show");
+    menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
